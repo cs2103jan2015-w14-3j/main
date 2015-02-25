06d6b73e6839fe26e2e35cbc1218db95130fd9d0
Sam-Mauris Yong <mauris@hotmail.sg>
2015-02-25 15:45:46 +0800

    added unit test for TasmaController
    working with issue #29

---
+++ test/com/tasma/TasmaControllerTest.java
@@ -0,0 +1,28 @@
+package com.tasma;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TasmaControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+}
