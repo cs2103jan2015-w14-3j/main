070f4e405861cdb2f8073c664042313eca63eae7
ManikaAgarwal <manika615@gmail.com>
2015-03-04 00:21:57 +0800

    added switch case and method stubs

--- src/com/tasma/TasmaParser.java
+++ src/com/tasma/TasmaParser.java
@@ -8,10 +8,47 @@ import java.util.ArrayList;
  */
 
 public class TasmaParser {
-	
-	public ArrayList<String> parseCommand() {
-		
+
+	public ArrayList<String> parseCommand(String command, CommandType type) {
+		switch (type) {
+		case ADD : 
+			return parseAdd(command);
+		case SEARCH :
+			return parseSearch(command);
+		case LIST :
+			return parseList(command);
+		case MARK :
+			return parseMark(command);
+		case ARCHIVE :
+			return parseArchive(command);
+		default :
+			break;	
+		}
 		return null;
 	}
 
+	private ArrayList<String> parseAdd(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ArrayList<String> parseSearch(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ArrayList<String> parseList(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ArrayList<String> parseMark(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ArrayList<String> parseArchive(String command) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
