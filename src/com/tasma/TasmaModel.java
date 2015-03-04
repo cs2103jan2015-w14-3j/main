9f2558293253c1c6218a26c477030e05e4a22575
He Buwei <a0119434@nus.edu.sg>
2015-03-04 14:53:29 +0800

    added class TasmaTask

--- src/com/tasma/TasmaModel.java
+++ src/com/tasma/TasmaModel.java
@@ -1,8 +1,23 @@
 package com.tasma;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+
 /**
  * 
  */
 public class TasmaModel {
+	
+	ArrayList tasks;
+	
+	public void loadFromFile() {
+		
+	}
+	
+	
+	public void undoCommand() {
+		
+	}
 	
 }
---
+++ src/com/tasma/TasmaTask.java
@@ -0,0 +1,9 @@
+package com.tasma;
+
+import java.util.Date;
+
+public class TasmaTask {
+	
+	String description;
+	Date deadline;
+}
