eefd9b0e5603ce44730ef4c0aed1f2bf01a22a80
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-05 18:41:54 +0800

    added controller initialise in main

--- src/com/tasma/TasmaApp.java
+++ src/com/tasma/TasmaApp.java
@@ -8,11 +8,13 @@ public class TasmaApp {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		TasmaController controller = new TasmaController();
+		final TasmaController controller = new TasmaController();
+		controller.initialize();
 		EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				try {
-					TasmaUI frame = new TasmaUI();
+					TasmaUI frame = new TasmaUI(controller);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
