96e020fc9a6da3543ff670633d92baabffb016bb
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-07 12:26:59 +0800

    added TasmaConsoleUI

---
+++ src/com/tasma/TasmaConsoleUI.java
@@ -0,0 +1,25 @@
+package com.tasma;
+
+import java.util.ArrayList;
+
+public class TasmaConsoleUI implements TasmaUserInterface {
+
+	@Override
+	public void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayTasks(ArrayList<Task> tasks) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void displayMessage(String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
