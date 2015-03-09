21c2349bc0338e5b7d04e2632159244564a5e8de
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 17:10:06 +0800

    added empty argument message for add command

--- src/com/tasma/UIMessage.java
+++ src/com/tasma/UIMessage.java
@@ -2,6 +2,7 @@ package com.tasma;
 
 public class UIMessage {
 	public static final String COMMAND_ADD_SUCCESS = "Your task \"%s\" has been successfully created as task #%s";
+	public static final String COMMAND_ADD_ARG_EMPTY = "The <add> command requires the details of the task you wish to add. Try:\n\tadd <details>";
 	
 	public static final String COMMAND_MARK_SUCCESS = "Your task #%s \"%s\" has been successfully marked as done.";
 
