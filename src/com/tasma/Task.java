8e1e17258e347e6e09ccec4d2677a81232721b9d
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 15:33:08 +0800

    added default empty strings for Task

--- src/com/tasma/Task.java
+++ src/com/tasma/Task.java
@@ -3,9 +3,9 @@ package com.tasma;
 import java.util.Date;
 
 public class Task {
-	private String taskId;
-	private String details;
-	private String location;
+	private String taskId = "";
+	private String details = "";
+	private String location = "";
 	private Date startDateTime;
 	private Date endDateTime;
 	private boolean isDone;
