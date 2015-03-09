92afece5aebaaff60116a4695855ad947d47a0f3
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 14:40:53 +0800

    implement Gson saving tasks to file

--- src/com/tasma/Storage.java
+++ src/com/tasma/Storage.java
@@ -1,15 +1,35 @@
 package com.tasma;
 
 import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import com.google.gson.Gson;
 
 public class Storage {
-	// TODO: to implement save method
+	protected static final String FILENAME = "tasks.json";
+	
 	public void save(ArrayList<Task> tasks) {
-		
+		Gson gson = new Gson();
+		String json = gson.toJson(tasks);
+    
+	    try {
+		    FileWriter writer = new FileWriter(FILENAME);  
+		    writer.write(json);  
+		    writer.close();  
+	    
+	    } catch (IOException e) {  
+		    e.printStackTrace();  
+	    }
 	}
 
 	// TODO: to implement load method
 	public ArrayList<Task> load() {
-		return new ArrayList<Task>();
+		ArrayList<Task> tasks = new ArrayList<Task>();
+		File file = new File(FILENAME);
+		if (file.exists()) {
+			
+		}
+		return tasks;
 	}
 }
