a0296364d6cb959eb963f0d12f18f46843bfb469
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 13:41:18 +0800

    renamed constant in UI message

--- src/com/tasma/UIMessage.java
+++ src/com/tasma/UIMessage.java
@@ -1,5 +1,5 @@
 package com.tasma;
 
 public class UIMessage {
-	public static final String ADD_SUCCESS = "Your task \"%s\" has been successfully created as task #%s";
+	public static final String COMMAND_ADD_SUCCESS = "Your task \"%s\" has been successfully created as task #%s";
 }
