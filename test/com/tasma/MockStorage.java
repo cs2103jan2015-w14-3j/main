f22b267b18669439a6bfac5265783d3d7a787569
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 15:41:18 +0800

    updated mock storage task list handling

--- test/com/tasma/MockStorage.java
+++ test/com/tasma/MockStorage.java
@@ -3,13 +3,15 @@ package com.tasma;
 import java.util.Hashtable;
 
 public final class MockStorage extends Storage {
+	protected Hashtable<String, Task> tasks = new Hashtable<String, Task>();
+	
 	@Override
 	public void save(Hashtable<String, Task> tasks) {
+		this.tasks = tasks;
 	}
 
 	@Override
 	public Hashtable<String, Task> load() {
-		Hashtable<String, Task> tasks = new Hashtable<String, Task>();
 		return tasks;
 	}
 }
