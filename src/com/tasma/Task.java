70179984b28265177b55e552500b71675478e194
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 08:30:10 +0800

    used eclipse to generate getter setter

--- src/com/tasma/Task.java
+++ src/com/tasma/Task.java
@@ -9,4 +9,57 @@ public class Task {
 	private Date startDateTime;
 	private Date endDateTime;
 	private boolean isDone;
+	
+	public Task(String taskId, String details) {
+		this.taskId = taskId;
+		this.details = details;
+	}
+	
+	public String getTaskId() {
+		return taskId;
+	}
+	
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getStartDateTime() {
+		return startDateTime;
+	}
+
+	public void setStartDateTime(Date startDateTime) {
+		this.startDateTime = startDateTime;
+	}
+
+	public Date getEndDateTime() {
+		return endDateTime;
+	}
+
+	public void setEndDateTime(Date endDateTime) {
+		this.endDateTime = endDateTime;
+	}
+
+	public boolean isDone() {
+		return isDone;
+	}
+
+	public void setDone(boolean isDone) {
+		this.isDone = isDone;
+	}
 }
