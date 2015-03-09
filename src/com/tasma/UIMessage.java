9bf063f334b9ef06cbb6ab13e53cead7af3ceab4
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 13:41:40 +0800

    invalid command message

--- src/com/tasma/UIMessage.java
+++ src/com/tasma/UIMessage.java
@@ -2,4 +2,6 @@ package com.tasma;
 
 public class UIMessage {
 	public static final String COMMAND_ADD_SUCCESS = "Your task \"%s\" has been successfully created as task #%s";
+	
+	public static final String COMMAND_INVALID = "The command you have entered is invalid. Please try again.";
 }
