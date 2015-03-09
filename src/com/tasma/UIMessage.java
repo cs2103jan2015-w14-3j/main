d659af59d79f6ac6966fdae7a59cecf4ced8c5d6
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 15:19:04 +0800

    added mark success message constant

--- src/com/tasma/UIMessage.java
+++ src/com/tasma/UIMessage.java
@@ -3,5 +3,7 @@ package com.tasma;
 public class UIMessage {
 	public static final String COMMAND_ADD_SUCCESS = "Your task \"%s\" has been successfully created as task #%s";
 	
+	public static final String COMMAND_MARK_SUCCESS = "Your task #%s \"%s\" has been successfully marked as done.";
+	
 	public static final String COMMAND_INVALID = "The command you have entered is invalid. Please try again.";
 }
