b0b92f242de7e0a077c5fc6fd310bb10f7549f83
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-09 14:46:58 +0800

    finished implementing storage

--- src/com/tasma/Storage.java
+++ src/com/tasma/Storage.java
@@ -3,8 +3,10 @@ package com.tasma;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 public class Storage {
 	protected static final String FILENAME = "tasks.json";
@@ -16,19 +18,24 @@ public class Storage {
 	    try {
 		    FileWriter writer = new FileWriter(FILENAME);  
 		    writer.write(json);  
-		    writer.close();  
-	    
+		    writer.close();
 	    } catch (IOException e) {  
-		    e.printStackTrace();  
+			// TODO: work on exception handling
 	    }
 	}
 
-	// TODO: to implement load method
 	public ArrayList<Task> load() {
 		ArrayList<Task> tasks = new ArrayList<Task>();
 		File file = new File(FILENAME);
 		if (file.exists()) {
-			
+			try {
+				FileReader reader = new FileReader(file);
+				Gson gson = new Gson();
+				tasks = gson.fromJson(reader, new TypeToken<ArrayList<Task>>() {}.getType());
+				reader.close();
+			}catch(IOException e) {
+				// TODO: work on exception handling
+			}
 		}
 		return tasks;
 	}
