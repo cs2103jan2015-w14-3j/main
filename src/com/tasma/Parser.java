8fea09c53ec47e0938a4a9350810c3465abdc6cb
ManikaAgarwal <manika615@gmail.com>
2015-03-09 15:33:41 +0800

    dummy parse for "next"

--- src/com/tasma/Parser.java
+++ src/com/tasma/Parser.java
@@ -1,7 +1,5 @@
 package com.tasma;
 
-import java.util.ArrayList;
-
 /**
  * @author Manika Agarwal
  *
@@ -9,52 +7,97 @@ import java.util.ArrayList;
 
 public class Parser {
 	private Task parsedTask;
-	
+
 	private static final String EMPTY_STRING = "";
 	private static String taskDetails; 
-	
+
 	public Task parse(String details) {
 		parsedTask = new Task();
 		taskDetails = details;
-		
+
 		getWhat();
-		
+
 		getWhen();
-		
+
 		getWhere();
-		
+
 		return parsedTask;
 	}
 
 
 	private void getWhat() {
-		String[] keywords = {"on", "at", "in"};
-		int keywordsSize = 3, index = 0; 
-		
+		final String[] keywords = {"on", "at", "in"};
+		final int keywordsSize = 3;
+		int index = 0; 
+
 		if (!taskDetails.contains("on") || !taskDetails.contains("at")) {  //command does not contain when or where details
 			taskDetails = EMPTY_STRING;
 			//still in preliminary phases as task may not contain 'on' or 'at' but still have when and/or where details
 		} else {
 			int i;
-			
-			for (i = 0; i < 3; i++) {
+
+			for (i = 0; i < keywordsSize; i++) {
 				if (taskDetails.contains(keywords[i])) {
 					index = taskDetails.indexOf(keywords[i]);		
 				}
 			}
 		}
-		
+
 		parsedTask.setDetails(taskDetails);
 		taskDetails = taskDetails.substring(index); 
 	}
-	
+
 	private void getWhen() {
 		if (taskDetails != EMPTY_STRING) {
-			
+			final String[] keywords = {"on", "from", "at", "by"};
+			final int keywordsSize = 4; 
+
+			if (taskDetails.contains(keywords[0])) {  //date
+				int indexOn = taskDetails.indexOf(keywords[0]);
+
+				if (taskDetails.contains(keywords[1])) {
+					int indexFrom = taskDetails.indexOf(keywords[1]);
+					String dateOfTask = taskDetails.substring(indexOn + 3, indexFrom - 1);
+					
+					if (dateOfTask.contains("next")) {
+						int indexNext = taskDetails.indexOf("next");
+						String date = taskDetails.substring(indexNext + "next".length());
+						
+						switch(date) {
+						case "mon" : 
+							parsedTask.setEndDateTime();
+						case "tues" : 
+							parsedTask.setEndDateTime();
+						case "wed" : 
+							parsedTask.setEndDateTime();
+						case "thur" : 
+							parsedTask.setEndDateTime();
+						case "fri" : 
+							parsedTask.setEndDateTime();
+						case "sat" : 
+							parsedTask.setEndDateTime();
+						case "sun" : 
+							parsedTask.setEndDateTime();
+						default:
+							;
+						}
+						
+					}
+				} else if (taskDetails.contains(keywords[2])) {
+					int indexAt = taskDetails.indexOf(keywords[2]);
+				}
+
+
+			}
+			/*for (i = 0; i < keywordsSize; i++) {
+				if (taskDetails.contains(keywords[i])) {
+					index = taskDetails.indexOf(keywords[i]);	
+				}
+			}*/
 		}
 	}
-	
+
 	private void getWhere() {
-		
+
 	}	
 }
