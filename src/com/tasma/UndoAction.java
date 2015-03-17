ee55b80b0d6e980640b436f5ccdd624e2920214f
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-17 09:52:18 +0800

    updated undoaction to include properties and getter
    #64

--- src/com/tasma/UndoAction.java
+++ src/com/tasma/UndoAction.java
@@ -5,5 +5,19 @@
 package com.tasma;
 
 public class UndoAction {
-
+	private CommandType command;
+	private Task task;
+	
+	public UndoAction(CommandType command, Task task) {
+		this.command = command;
+		this.task = task;
+	}
+	
+	public CommandType getCommand() {
+		return command;
+	}
+	
+	public Task getTask() {
+		return task;
+	}
 }
