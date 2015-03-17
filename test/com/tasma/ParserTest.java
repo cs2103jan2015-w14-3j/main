ba1ab331de9c2ae8a316fb09a57872a2f2507404
ManikaAgarwal <manika615@gmail.com>
2015-03-17 21:44:53 +0800

    Created file to test parser functionality

---
+++ test/com/tasma/ParserTest.java
@@ -0,0 +1,14 @@
+package com.tasma;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class ParserTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
