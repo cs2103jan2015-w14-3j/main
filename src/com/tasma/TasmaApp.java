c2d3f69f8da67cef13f66fab7c84c99446ac8615
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-17 17:55:08 +0800

    added logger for tasmaapp

--- src/com/tasma/TasmaApp.java
+++ src/com/tasma/TasmaApp.java
@@ -4,8 +4,11 @@
 package com.tasma;
 
 import java.awt.EventQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TasmaApp {
+	private static final Logger logger = Logger.getLogger( Controller.class.getName() );
 	
 	/**
 	 * Launch the application.
@@ -16,11 +19,16 @@ public class TasmaApp {
 			public void run() {
 				try {
 					LoggerSetup.setup();
+					
+					logger.log(Level.FINE, "Initializing window & application");
 					TasmaGUI frame = new TasmaGUI();
 					frame.initialize();
+					
+					logger.log(Level.FINE, "Showing window and requesting command box focus.");
 					frame.setVisible(true);
 					frame.requestCommandBoxFocus();
 				} catch (Exception e) {
+					logger.log(Level.SEVERE, e.toString(), e);
 					e.printStackTrace();
 				}
 			}
