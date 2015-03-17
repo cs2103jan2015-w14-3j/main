85f9feb67d0d7e1817d1e17768bc51e72e7a8336
ManikaAgarwal <manika615@gmail.com>
2015-03-17 23:01:07 +0800

    added a passing test

--- test/com/tasma/ParserTest.java
+++ test/com/tasma/ParserTest.java
@@ -1,14 +1,29 @@
 package com.tasma;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
+import org.joda.time.DateTimeConstants;
+import org.joda.time.LocalDate;
 import org.junit.Test;
 
 public class ParserTest {
 	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		Parser caller = new Parser();
+		Task parsedTask = new Task();
+		parsedTask = caller.parse("do cs2105 on next mon");
+		Task temp = new Task();
+		
+		temp.setDetails("do cs2105");
+		
+		LocalDate d = new LocalDate();
+		d = d.plusWeeks(1);
+		d = d.withDayOfWeek(DateTimeConstants.MONDAY);	
+		temp.setEndDateTime(d);
+		
+		assertEquals(temp.getEndDateTime(), parsedTask.getEndDateTime());
+		assertEquals(temp.getDetails(), parsedTask.getDetails());
+		assertEquals(temp.getLocation(), parsedTask.getLocation());
 	}
-
 }
