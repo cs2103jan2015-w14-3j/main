153ff76d7bcfcc7a6c8e1f7f0f7c1d7bbbf9113e
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 17:41:03 +0800

    implemented the inputsplitter class

---
+++ src/com/tasma/commands/InputSplitter.java
@@ -0,0 +1,26 @@
+package com.tasma.commands;
+
+public class InputSplitter {
+	private static final String ARGUMENT_SPACE = " ";
+	private static final int ARGUMENT_SPACE_NOT_FOUND = -1;
+	
+	private String input;
+
+	public InputSplitter(String input) {
+		this.input = input;
+	}
+	
+	public String next() {
+		String result = "";
+		int intSpacePos = input.indexOf(ARGUMENT_SPACE);
+		if (intSpacePos != ARGUMENT_SPACE_NOT_FOUND) {
+			result = input.substring(0, intSpacePos);
+			input = input.substring(intSpacePos + 1);
+		}
+		return result;
+	}
+	
+	public String remainder() {
+		return input;
+	}
+}
