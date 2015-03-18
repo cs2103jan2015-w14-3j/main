9521dde521029d55fa7b53bc8647712503414d68
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 16:32:35 +0800

    added undonotsupportedexception

---
+++ src/com/tasma/commands/UndoNotSupportedException.java
@@ -0,0 +1,10 @@
+package com.tasma.commands;
+
+public class UndoNotSupportedException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8267692072568234883L;
+
+}
