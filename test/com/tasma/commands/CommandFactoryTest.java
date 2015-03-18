861ae017afd96c391d3be8aa6018ad6a44969594
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 18:11:02 +0800

    added more test for CommandFactory

--- test/com/tasma/commands/CommandFactoryTest.java
+++ test/com/tasma/commands/CommandFactoryTest.java
@@ -1,15 +1,29 @@
 package com.tasma.commands;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.tasma.MockStorage;
+import com.tasma.TaskCollection;
+import com.tasma.MockUserInterface;
+
 public class CommandFactoryTest {
 
+	private CommandFactory factory;
+	private MockUserInterface userInterface;
+	private MockStorage storage;
+	private TaskCollection collection;
+	
 	@Before
 	public void setUp() throws Exception {
+		userInterface = new MockUserInterface();
+		storage = new MockStorage();
+		collection = new TaskCollection(storage);
+		factory = new CommandFactory(userInterface, collection);
 	}
 
 	@After
@@ -17,8 +31,21 @@ public class CommandFactoryTest {
 	}
 
 	@Test
-	public void testGetCommand() {
-		fail("Not yet implemented");
+	public void testGetCommand1() {
+		CommandInterface command = factory.getCommand("add test");
+		assertThat(command, instanceOf(AddCommand.class));
+	}
+
+	@Test
+	public void testGetCommand2() {
+		CommandInterface command = factory.getCommand("list");
+		assertThat(command, instanceOf(ListCommand.class));
+	}
+
+	@Test
+	public void testGetCommand3() {
+		CommandInterface command = factory.getCommand("search test");
+		assertThat(command, instanceOf(SearchCommand.class));
 	}
 
 }
