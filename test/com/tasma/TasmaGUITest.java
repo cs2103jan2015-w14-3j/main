8c95efbc24b08731bc313683e95e711cf6ee60f5
Edward <angertrail@hotmail.com>
2015-03-18 09:19:34 +0800

    Added test to see if application is working

--- test/com/tasma/TasmaGUITest.java
+++ test/com/tasma/TasmaGUITest.java
@@ -3,12 +3,28 @@ package com.tasma;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import junit.framework.TestCase;
 
-public class TasmaGUITest {
+public class TasmaGUITest extends TestCase {
+	
+	TasmaGUI window;
+	
+	@Before
+	public void setUp() {
+		window = new TasmaGUI();
+		window.setVisible(true);
+	}
+	
+	@After
+	public void tearDown() {
+		window.dispose();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsShowing() {
+		assertTrue(window.isShowing());
 	}
 
 }
