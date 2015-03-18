87066d16ad4d7ec4202864fda1c69ec84f7a98be
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 15:00:46 +0800

    added the addcommand

---
+++ src/com/tasma/commands/AddCommand.java
@@ -0,0 +1,17 @@
+package com.tasma.commands;
+
+public class AddCommand implements CommandInterface {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
