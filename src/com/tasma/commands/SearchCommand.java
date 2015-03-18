67ac30b02babb760c161ce3f97fea5c4cd45397b
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 16:35:38 +0800

    added search command

---
+++ src/com/tasma/commands/SearchCommand.java
@@ -0,0 +1,36 @@
+package com.tasma.commands;
+
+import java.util.Collection;
+
+import com.tasma.Task;
+import com.tasma.TaskCollection;
+import com.tasma.TasmaUserInterface;
+import com.tasma.UIMessage;
+
+public class SearchCommand extends AbstractCommand {
+
+	protected String query;
+	
+	public SearchCommand(TasmaUserInterface userInterface,
+			TaskCollection collection, String query) {
+		super(userInterface, collection);
+		this.query = query;
+	}
+
+	@Override
+	public void execute() throws Exception {
+		if (query.equals("")) {
+			// TODO: to handle empty search terms?
+		} else {
+			Collection<Task> resultList = collection.search(query);
+			userInterface.displayTasks(resultList);
+			userInterface.displayMessage(String.format(UIMessage.COMMAND_SEARCH_RESULT, resultList.size(), query));
+		}
+	}
+
+	@Override
+	public void undo() throws Exception {
+		throw new UndoNotSupportedException();
+	}
+
+}
