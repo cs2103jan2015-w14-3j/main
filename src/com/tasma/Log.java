23168d17d408b87287ad0f800512f6131f853e3d
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 12:39:15 +0800

    added comment for log

--- src/com/tasma/Log.java
+++ src/com/tasma/Log.java
@@ -6,6 +6,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
+/**
+ * This is a singleton service for all the classes to write to a common log file.
+ * @author Yong Shan Xian <ysx@u.nus.edu>
+ */
 public class Log {
 	private Level level;
 	private FileHandler logFile;
