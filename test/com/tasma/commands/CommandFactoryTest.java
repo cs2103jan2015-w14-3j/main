e1de774ce65d95f289bbae06f47628c1e85ec2dc
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 17:55:25 +0800

    added commandfactory test

---
+++ test/com/tasma/commands/CommandFactoryTest.java
@@ -0,0 +1,24 @@
+package com.tasma.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommandFactoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetCommand() {
+		fail("Not yet implemented");
+	}
+
+}
