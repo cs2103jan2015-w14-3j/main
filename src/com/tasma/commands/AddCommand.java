44ea1fede8488e5697c9d6dcffabaac062e7f3bc
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-18 16:30:42 +0800

    improve the addcommand class

--- src/com/tasma/commands/AddCommand.java
+++ src/com/tasma/commands/AddCommand.java
@@ -1,17 +1,41 @@
 package com.tasma.commands;
 
-public class AddCommand implements CommandInterface {
+import com.tasma.Parser;
+import com.tasma.Task;
+import com.tasma.TaskCollection;
+import com.tasma.TasmaUserInterface;
+import com.tasma.UIMessage;
 
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-
+public class AddCommand extends AbstractCommand {
+	
+	protected String details;
+	protected Task resultTask;
+	
+	public AddCommand(TasmaUserInterface userInterface, TaskCollection tasks, String details) {
+		super(userInterface, tasks);
+		this.details = details;
 	}
 
 	@Override
-	public void undo() {
-		// TODO Auto-generated method stub
+	public void execute() throws Exception {
+		if (details.equals("")) {
+			userInterface.displayMessage(UIMessage.COMMAND_ADD_ARG_EMPTY);
+		} else {
+			assert !details.equals("");
+			
+			Parser parser = new Parser();
+			Task task = parser.parse(details);
+			collection.create(task);
+			userInterface.displayMessage(String.format(UIMessage.COMMAND_ADD_SUCCESS, task.getDetails(), task.getTaskId()));
+			resultTask = task.clone();
+		}
+	}
 
+	@Override
+	public void undo() throws Exception {
+		assert resultTask != null;
+		
+		collection.delete(resultTask.getTaskId());
 	}
 
 }
