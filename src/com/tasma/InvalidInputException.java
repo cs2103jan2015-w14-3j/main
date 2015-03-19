66d9e5e4038a50dc97e8f067a9f65b93538de885
ManikaAgarwal <manika615@gmail.com>
2015-03-19 22:21:38 +0800

    created class InvalidInputException

---
+++ src/com/tasma/InvalidInputException.java
@@ -0,0 +1,18 @@
+package com.tasma;
+
+public class InvalidInputException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2968409273587420542L;
+
+	public InvalidInputException() {}
+
+    //Constructor that accepts a message
+    public InvalidInputException(String message)
+    {
+       super(message);
+    }
+
+}
