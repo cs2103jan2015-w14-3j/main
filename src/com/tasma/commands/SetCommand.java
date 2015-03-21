8b4544b25cdbb2704cd817814fa2285ce277ef07
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-22 00:58:33 +0800

    updated SetCommand to use AbstractCommand extension

--- src/com/tasma/commands/SetCommand.java
+++ src/com/tasma/commands/SetCommand.java
@@ -1,5 +1,20 @@
 package com.tasma.commands;
 
-public class SetCommand {
+import com.tasma.TaskCollection;
+import com.tasma.TasmaUserInterface;
+
+public class SetCommand extends AbstractCommand {
+
+	public SetCommand(TasmaUserInterface userInterface,
+			TaskCollection collection) {
+		super(userInterface, collection);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
