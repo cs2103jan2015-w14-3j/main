f4f79a396bf543b00305c764d3c79f0ff624cc07
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-22 11:34:47 +0800

    added implementation of SetCommand

--- src/com/tasma/commands/SetCommand.java
+++ src/com/tasma/commands/SetCommand.java
@@ -1,24 +1,36 @@
-package com.tasma.commands;
-
-import com.tasma.TaskCollection;
-import com.tasma.TasmaUserInterface;/**
+/**
  * Tasma Task Manager
  */
 //@author A0132763
+package com.tasma.commands;
 
+import com.tasma.Config;
+import com.tasma.TaskCollection;
+import com.tasma.TasmaUserInterface;
 
-public class SetCommand extends AbstractCommand {
+public class SetCommand extends AbstractCommand implements UndoableCommandInterface {
+	
+	private String key;
+	private String previousValue;
+	private String newValue;
 
 	public SetCommand(TasmaUserInterface userInterface,
-			TaskCollection collection) {
+			TaskCollection collection, String key, String value) {
 		super(userInterface, collection);
-		// TODO Auto-generated constructor stub
+		this.key = key;
+		this.newValue = value;
 	}
 
 	@Override
 	public void execute() throws Exception {
-		// TODO Auto-generated method stub
-		
+		Config config = Config.getInstance();
+		previousValue = config.getProperty(key);
+		config.setProperty(key, newValue);
+	}
+
+	@Override
+	public void undo() throws Exception {
+		config.setProperty(key, previousValue);
 	}
 
 }
