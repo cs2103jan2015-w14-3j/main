e6d4edf944acc9700a1dff0aae147dada8a792fc
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-22 12:22:58 +0800

    added default provider interface

---
+++ src/com/tasma/config/DefaultProviderInterface.java
@@ -0,0 +1,11 @@
+/**
+ * Tasma Task Manager
+ */
+//@author A0132763
+package com.tasma.config;
+
+import java.util.Map;
+
+public interface DefaultProviderInterface {
+	public Map<String, String> defaults(); 
+}
