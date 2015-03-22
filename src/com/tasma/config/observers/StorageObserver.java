68f8fdf633286a7303b680ab7f1ba907a0c820ce
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-22 12:36:12 +0800

    added StorageObserver class

---
+++ src/com/tasma/config/observers/StorageObserver.java
@@ -0,0 +1,22 @@
+package com.tasma.config.observers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.tasma.config.ChangeObserverInterface;
+import com.tasma.config.DefaultProviderInterface;
+
+public class StorageObserver implements ChangeObserverInterface, DefaultProviderInterface {
+
+	@Override
+	public void notify(String key, String oldValue, String newValue) {
+		// TODO to move the tasks.json file and update the storage path
+	}
+	
+	public Map<String, String> defaults() {
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("storage", System.getProperty("user.dir"));
+		return result;
+	}
+
+}
