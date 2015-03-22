2ac78a53f01ac369a1987ae6ddb1f2a7aa491fe4
He Buwei <a0119434@nus.edu.sg>
2015-03-22 09:46:19 +0800

    added constructor for Config

--- src/com/tasma/Config.java
+++ src/com/tasma/Config.java
@@ -1,11 +1,26 @@
 package com.tasma;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class Config {
 	
 	private static Config instance = null;
 	
+	private Properties properties = null;
+	private File configFile;
+	
+	private Config() throws IOException {
+		configFile = new File("config.properties");
+	    if(!configFile.exists()) {
+	    	configFile.createNewFile();
+	    }
+	    FileInputStream in = new FileInputStream(configFile);
+	    properties.load(in);
+	}
+    
 	protected static Config getInstance() {
 		if (instance == null) {
 			try {
