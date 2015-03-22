70fdde06c1862855d3cd259c61401931480359ad
He Buwei <a0119434@nus.edu.sg>
2015-03-22 10:03:05 +0800

    added get and set methods for Config

--- src/com/tasma/Config.java
+++ src/com/tasma/Config.java
@@ -9,7 +9,7 @@ public class Config {
 	
 	private static Config instance = null;
 	
-	private Properties properties = null;
+	private Properties properties;
 	private File configFile;
 	
 	private Config() throws IOException {
@@ -19,6 +19,7 @@ public class Config {
 	    }
 	    FileInputStream in = new FileInputStream(configFile);
 	    properties.load(in);
+	    in.close();
 	}
     
 	protected static Config getInstance() {
@@ -30,5 +31,22 @@ public class Config {
 			}
 		}
 		return instance;
+	}
+	
+	/**
+	 * Searches for the property with the specified key in this property list.
+	 * Returns null if the property is not found.
+	 */
+	public String getProperty(String key) {
+		return properties.getProperty(key);
+	}
+	
+	/**
+	 * Sets the property based on specified key and value.
+	 * @return the previous value of the specified key in this property list, 
+	 *         or null if it did not have one
+	 */
+	public String setProperty(String key, String value) {
+		return (String) properties.setProperty(key, value);
 	}
 }
