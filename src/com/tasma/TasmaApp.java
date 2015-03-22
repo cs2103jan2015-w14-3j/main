bfbb1a4ad456b6ad301bddbb7d7d9f79d89d209d
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-22 11:57:24 +0800

    improved code in TasmaApp

--- src/com/tasma/TasmaApp.java
+++ src/com/tasma/TasmaApp.java
@@ -7,29 +7,29 @@ import java.awt.EventQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class TasmaApp {
+public class TasmaApp implements Runnable {
 	private static final Logger logger = Log.getLogger( TasmaApp.class.getName() );
 	
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				try {
-					logger.log(Level.FINE, "Initializing window & application");
-					TasmaGUI frame = new TasmaGUI();
-					frame.initialize();
-					
-					logger.log(Level.FINE, "Showing window and requesting command box focus.");
-					frame.setVisible(true);
-					frame.requestCommandBoxFocus();
-				} catch (Exception e) {
-					logger.log(Level.SEVERE, e.toString(), e);
-					e.printStackTrace();
-				}
-			}
-		});
+		EventQueue.invokeLater(new TasmaApp());
+	}
+	
+	@Override
+	public void run() {
+		try {
+			logger.log(Level.FINE, "Initializing window & application");
+			TasmaGUI frame = new TasmaGUI();
+			frame.initialize();
+			
+			logger.log(Level.FINE, "Showing window and requesting command box focus.");
+			frame.setVisible(true);
+			frame.requestCommandBoxFocus();
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, e.toString(), e);
+			e.printStackTrace();
+		}
 	}
 }
