4682a0c1ebaa27927ad2f332ad2282f959a1590d
ManikaAgarwal <manika615@gmail.com>
2015-03-24 22:29:39 +0800

    added test4 to check flow for invalid date

--- test/com/tasma/ParserTest.java
+++ test/com/tasma/ParserTest.java
@@ -7,88 +7,102 @@ import org.joda.time.DateTime;
 import org.junit.Test;
 
 public class ParserTest {
-	
+
 	@Test
 	public void test1() {
 		Parser caller = new Parser();
 		Task parsedTask = new Task();
-		
+
 		try {
 			parsedTask = caller.parse("do cs2105 on next mon");
 		} catch (InvalidInputException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		Task temp = new Task();
-		
+
 		temp.setDetails("do cs2105");
-		
+
 		DateTime d = initializeDateTime();
 		d = d.plusWeeks(1);
 		d = d.withDayOfWeek(DateTimeConstants.MONDAY);	
 		temp.setEndDateTime(d);
-		
+
 		assertEquals(temp.getDetails(), parsedTask.getDetails());
 		assertEquals(temp.getEndDateTime(), parsedTask.getEndDateTime());
 		assertEquals(temp.getLocation(), parsedTask.getLocation());
 	}
-	
+
 	@Test
 	public void test2() {
 		Parser caller = new Parser();
 		Task parsedTask = new Task();
-		
+
 		try {
 			parsedTask = caller.parse("do cs2105 on mon");
 		} catch (InvalidInputException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		Task temp = new Task();
-		
+
 		temp.setDetails("do cs2105");
 
 		DateTime d = initializeDateTime();
 		d = d.withDayOfWeek(DateTimeConstants.MONDAY);	
-		
+
 		if (d.isBefore(new DateTime())) {
 			d = d.plusWeeks(1);
 		}
-		
+
 		temp.setEndDateTime(d);
-		
+
 		assertEquals(temp.getDetails(), parsedTask.getDetails());
 		assertEquals(temp.getEndDateTime(), parsedTask.getEndDateTime());
 		assertEquals(temp.getLocation(), parsedTask.getLocation());
 	}
-	
+
 	@Test
 	public void test3() {
 		Parser caller = new Parser();
 		Task parsedTask = new Task();
-		
+
 		try {
 			parsedTask = caller.parse("do cs2105 on next mon at ALL");
 		} catch (InvalidInputException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		Task temp = new Task();
-		
+
 		temp.setDetails("do cs2105");
 
 		DateTime d = initializeDateTime();
 		d = d.plusWeeks(1);
 		d = d.withDayOfWeek(DateTimeConstants.MONDAY);	
 		temp.setEndDateTime(d);
-		
+
 		temp.setLocation("ALL");
-		
+
 		assertEquals(temp.getDetails(), parsedTask.getDetails());
 		assertEquals(temp.getEndDateTime(), parsedTask.getEndDateTime());
 		assertEquals(temp.getLocation(), parsedTask.getLocation());
 	}
-	
+
+	//Invalid case for date parsing
+	@Test
+	public void test4() {
+		Parser caller = new Parser();
+		Task parsedTask = new Task();
+
+		try {
+			parsedTask = caller.parse("do cs2105 on 22-15-15 at 2pm at ALL");
+		} catch (Exception e) {
+			assertTrue(e instanceof InvalidInputException);
+			assertEquals("Invalid date", e.getMessage()); 
+		}
+	}
+
 	private DateTime initializeDateTime() {
 		DateTime d = new DateTime();
 		d = d.withHourOfDay(0);
@@ -96,4 +110,6 @@ public class ParserTest {
 		d = d.withMillisOfDay(0);
 		return d;
 	}
+
+
 }
