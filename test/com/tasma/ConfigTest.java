9fc5b48aa42c6349ac3346fc99b5278d6aae72f8
He Buwei <a0119434@nus.edu.sg>
2015-03-25 14:37:26 +0800

    Added unit tests for Config

---
+++ test/com/tasma/ConfigTest.java
@@ -0,0 +1,33 @@
+package com.tasma;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tasma.config.Config;
+
+
+
+public class ConfigTest {
+
+	private Config config;
+	
+	@Before
+	public void setUp() throws Exception {
+		config = Config.getInstance();
+	}
+
+	@Test
+	public void testSetNGet() throws Exception {
+		config.setProperty("k1", "v1");
+		assertEquals(config.getProperty("k1"), "v1");
+	}
+	
+	@Test
+	public void testDuplicateSet() throws Exception {
+		config.setProperty("k2", "v2");
+		config.setProperty("k2", "v22");
+		assertEquals(config.getProperty("k2"), "v22");
+	}
+}
