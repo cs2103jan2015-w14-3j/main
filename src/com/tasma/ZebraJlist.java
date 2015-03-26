2f5964e966d28621d1eec3c8cd20a5fef7bbb7b1
Edward <angertrail@hotmail.com>
2015-03-26 16:10:57 +0800

    added constructors for the class

--- src/com/tasma/ZebraJlist.java
+++ src/com/tasma/ZebraJlist.java
@@ -1,5 +1,23 @@
 package com.tasma;
 
-public class ZebraJlist {
-
+public class ZebraJList extends javax.swing.JList{
+	
+	private java.awt.Color rowColors[] = new java.awt.Color[2];
+    private boolean drawStripes = false;
+    
+    public ZebraJList() {};
+    
+    public ZebraJList( javax.swing.ListModel dataModel ) {
+        super( dataModel );
+    }
+    public ZebraJList( Object[] listData )
+    {
+        super( listData );
+    }
+    public ZebraJList( java.util.Vector<?> listData )
+    {
+        super( listData );
+    }
+    
+    
 }
