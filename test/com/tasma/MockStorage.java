d2ce0479ee2a651c58a5d421827d08af64f2ddd0
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-27 17:36:38 +0800

    added test fix

--- test/com/tasma/MockStorage.java
+++ test/com/tasma/MockStorage.java
@@ -3,6 +3,12 @@ package com.tasma;
 import java.util.Hashtable;
 
 public final class MockStorage extends Storage {
+	
+	public MockStorage() throws Exception {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 	protected Hashtable<String, Task> tasks = new Hashtable<String, Task>();
 	
 	@Override
