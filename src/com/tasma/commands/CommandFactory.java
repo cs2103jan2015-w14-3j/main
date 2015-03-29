cd1397fc2ef03c342f93a85bcbc9a21ff698e14d
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-30 01:49:21 +0800

    added normalize index input

--- src/com/tasma/commands/CommandFactory.java
+++ src/com/tasma/commands/CommandFactory.java
@@ -4,7 +4,10 @@
 //@author A0132763
 package com.tasma.commands;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import com.tasma.InvalidInputException;
 import com.tasma.Task;
 import com.tasma.TaskCollection;
 import com.tasma.TasmaUserInterface;
@@ -13,13 +16,14 @@ public class CommandFactory {
 
 	protected TasmaUserInterface userInterface;
 	protected TaskCollection collection;
+	protected LinkedList<Task> currentState = new LinkedList<Task>();
 	
 	public CommandFactory(TasmaUserInterface userInterface, TaskCollection collection) {
 		this.userInterface = userInterface;
 		this.collection = collection;
 	}
 
-	public CommandInterface getCommand(String input, List<Task> state) {
+	public CommandInterface getCommand(String input) throws InvalidInputException {
 		CommandInterface result = null;
 		InputSplitter splitter = new InputSplitter(input);
 		String command = splitter.next();
@@ -27,22 +31,22 @@ public class CommandFactory {
 		CommandType commandType = normalizeCommand(command);
 		switch (commandType) {
 			case ADD:
-				result = new AddCommand(userInterface, collection, state, splitter.remainder());
+				result = new AddCommand(userInterface, collection, currentState, splitter.remainder());
 				break;
 			case SEARCH:
 				result = new SearchCommand(userInterface, collection, splitter.remainder());
 				break;
 			case LIST:
-				result = new ListCommand(userInterface, collection, state, splitter.remainder());
+				result = new ListCommand(userInterface, collection, currentState, splitter.remainder());
 				break;
 			case MARK:
-				result = new MarkCommand(userInterface, collection, state, Integer.parseInt(splitter.remainder()));
+				result = new MarkCommand(userInterface, collection, currentState, normalizeIndexInput(splitter.remainder()));
 				break;
 			case EDIT:
-				result = new EditCommand(userInterface, collection, state, Integer.parseInt(splitter.next()), splitter.remainder());
+				result = new EditCommand(userInterface, collection, currentState, normalizeIndexInput(splitter.next()), splitter.remainder());
 				break;
 			case ARCHIVE:
-				result = new ArchiveCommand(userInterface, collection, state, Integer.parseInt(splitter.remainder()));
+				result = new ArchiveCommand(userInterface, collection, currentState, normalizeIndexInput(splitter.remainder()));
 				break;
 			case SET:
 				result = new SetCommand(userInterface, collection, splitter.next(), splitter.remainder());
@@ -60,6 +64,19 @@ public class CommandFactory {
 				// probably an invalid command
 				result = new InvalidCommand(userInterface, collection);
 				break;
+		}
+		return result;
+	}
+	
+	protected int normalizeIndexInput(String strIndex) throws InvalidInputException {
+		int result = -1;
+		try {
+			result = Integer.parseInt(strIndex) - 1;
+		} catch (NumberFormatException ex) {
+		}
+
+		if (result < 0 || result > currentState.size() - 1) {
+			throw new InvalidInputException();
 		}
 		return result;
 	}
