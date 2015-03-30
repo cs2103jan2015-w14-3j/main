aa045d85a7e28fa87fc5bf26e50b9c5ba1b7f15e
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-30 15:21:29 +0800

    added command identifier class

--- src/com/tasma/commands/CommandFactory.java
+++ src/com/tasma/commands/CommandFactory.java
@@ -26,7 +26,7 @@ public class CommandFactory {
 		InputSplitter splitter = new InputSplitter(input);
 		String command = splitter.next();
 
-		CommandType commandType = normalizeCommand(command);
+		CommandType commandType = CommandIdentifier.normalize(command);
 		switch (commandType) {
 			case ADD:
 				result = new AddCommand(userInterface, collection, currentState, splitter.remainder());
@@ -77,61 +77,5 @@ public class CommandFactory {
 			throw new InvalidInputException();
 		}
 		return result;
-	}
-	
-	/**
-	 * Normalizes variation of a command into a specific CommandType for processing later
-	 * @param command The command to be interpreted.
-	 * @return Returns the command type if the command is valid, otherwise returns the CommandType.INVALID value.
-	 */
-	protected static CommandType normalizeCommand(String command) {
-		switch(command.trim().toLowerCase()) {
-			case "add":
-			case "a":
-			case "insert":
-			case "create":
-			case "c":
-				return CommandType.ADD;
-			case "set":
-				return CommandType.SET;
-			case "s":
-			case "search":
-			case "find":
-			case "f":
-			case "q":
-			case "query":
-				return CommandType.SEARCH;
-			case "e":
-			case "edit":
-			case "update":
-			case "change":
-				return CommandType.EDIT;
-			case "u":
-			case "upcoming":
-			case "up":
-			case "l":
-			case "list":
-				return CommandType.LIST;
-			case "undo":
-				return CommandType.UNDO;
-			case "m":
-			case "mark":
-			case "do":
-			case "done":
-				return CommandType.MARK;
-			case "arc":
-			case "archive":
-				return CommandType.ARCHIVE;
-			case "h":
-			case "help":
-				return CommandType.HELP;
-			case "t":
-			case "tutorial":
-				return CommandType.TUTORIAL;
-			case "exit":
-				return CommandType.EXIT;
-			
-		}
-		return CommandType.INVALID;
 	}
 }
