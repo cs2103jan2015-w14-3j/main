4037210d1f24d290fd369da03974d61a398aea9a
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-30 18:36:23 +0800

    added help add message and template

--- src/com/tasma/HelpMessage.java
+++ src/com/tasma/HelpMessage.java
@@ -1,5 +1,7 @@
 package com.tasma;
 
 public final class HelpMessage {
-
+	public static final String HELP_TEMPLATE = "Help information about <%s> command\n\n%s\n\nAliases:\n%s";
+	
+	public static final String HELP_ADD = "\tadd <task details>\n\nThe <add> command creates a new task for you based on the details you have entered. After entering the command, the list will be refreshed to show the new task in the list.";
 }
