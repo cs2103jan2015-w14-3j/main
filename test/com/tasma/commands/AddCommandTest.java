87d8a50b7cb603052ca8637769b4b2069e9f9e82
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-31 12:09:02 +0800

    added AddCommandTest

---
+++ test/com/tasma/commands/AddCommandTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.tasma.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author sam
+ *
+ */
+public class AddCommandTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * Test method for {@link com.tasma.commands.AddCommand#execute()}.
+	 */
+	@Test
+	public void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.tasma.commands.AddCommand#undo()}.
+	 */
+	@Test
+	public void testUndo() {
+		fail("Not yet implemented");
+	}
+
+}
