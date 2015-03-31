fe7a4a18906059eca62969c218894b3364403575
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-31 12:20:40 +0800

    added execute test

--- test/com/tasma/commands/AddCommandTest.java
+++ test/com/tasma/commands/AddCommandTest.java
@@ -4,10 +4,15 @@
 package com.tasma.commands;
 
 import static org.junit.Assert.*;
-
+import static org.hamcrest.CoreMatchers.*;
+import java.util.LinkedList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import com.tasma.MockStorage;
+import com.tasma.MockUserInterface;
+import com.tasma.Task;
+import com.tasma.TaskCollection;
 
 /**
  * @author sam
@@ -15,11 +20,23 @@ import org.junit.Test;
  */
 public class AddCommandTest {
 
+	private AddCommand command;
+	
+	private MockStorage storage;
+	
+	private TaskCollection collection;
+	
+	private MockUserInterface userInterface;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		userInterface = new MockUserInterface();
+		storage = new MockStorage();
+		collection = new TaskCollection(storage);
+		collection.loadFromFile();
 	}
 
 	/**
@@ -33,15 +50,20 @@ public class AddCommandTest {
 	 * Test method for {@link com.tasma.commands.AddCommand#execute()}.
 	 */
 	@Test
-	public void testExecute() {
-		fail("Not yet implemented");
+	public void testExecute() throws Exception {
+		assertEquals(0, storage.getTasks().size());
+		LinkedList<Task> state = new LinkedList<Task>();
+		command = new AddCommand(userInterface, collection, state, "send car to workshop on next tuesday");
+		command.execute();
+		assertEquals(1, storage.getTasks().size());
+		assertThat(storage.getTasks().get(0), instanceOf(Task.class));
 	}
 
 	/**
 	 * Test method for {@link com.tasma.commands.AddCommand#undo()}.
 	 */
 	@Test
-	public void testUndo() {
+	public void testUndo() throws Exception {
 		fail("Not yet implemented");
 	}
 
