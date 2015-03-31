4e4e55d27a357d81af0423e48a95822cf417a252
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-31 16:43:22 +0800

    created a palette for usage

---
+++ src/com/tasma/Palette.java
@@ -0,0 +1,12 @@
+package com.tasma;
+
+import java.awt.Color;
+
+public class Palette {
+
+	public static final Color MESSAGE_SUCCESS = new Color(86, 179, 114);
+	public static final Color MESSAGE_WARNING = new Color(217, 217, 61);
+	public static final Color MESSAGE_DANGER = new Color(217, 71, 61);
+	public static final Color MESSAGE_GRAYED = new Color(60, 60, 60);
+
+}
