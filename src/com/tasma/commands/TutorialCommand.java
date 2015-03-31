b58e7dd448f0c734b470e8d5fbd7a6cfcd2e60a1
Sam-Mauris Yong <mauris@hotmail.sg>
2015-03-31 13:30:46 +0800

    implemented tutorial display

--- src/com/tasma/commands/TutorialCommand.java
+++ src/com/tasma/commands/TutorialCommand.java
@@ -9,6 +9,10 @@ import com.tasma.TasmaUserInterface;
 
 public class TutorialCommand extends AbstractCommand {
 
+	private static final String TUTORIAL_MESSAGE = "Hello there! Welcome to Tasma, your personal task manager.\n\n"
+			+ "You can start creating new tasks by entering the 'add' command followed by details of your task. More information on the 'add' command can be found using the 'help add' command.\n\n"
+			+ "The 'help' command will show you a list of commands and features available in Tasma.";
+	
 	public TutorialCommand(TasmaUserInterface userInterface,
 			TaskCollection collection) {
 		super(userInterface, collection);
@@ -16,7 +20,7 @@ public class TutorialCommand extends AbstractCommand {
 
 	@Override
 	public void execute() throws Exception {
-		// TODO to implement tutorial
+		userInterface.displayMessage(TUTORIAL_MESSAGE);
 	}
 
 }
