e4e36afacb820158ccf8b0c9bc828ff144b25fa4
He Buwei <a0119434@nus.edu.sg>
2015-04-01 13:21:01 +0800

    added TaskTest

---
+++ test/com/tasma/TaskTest.java
@@ -0,0 +1,25 @@
+package com.tasma;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TaskTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	@Test
+	public void testGetStringEndDateTime() {
+		
+	}
+
+}
