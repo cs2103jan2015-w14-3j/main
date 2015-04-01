1de508698df4cad395d9a3c7adb137156358f245
He Buwei <a0119434@nus.edu.sg>
2015-04-01 13:40:11 +0800

    added more test cases for Task

--- test/com/tasma/TaskTest.java
+++ test/com/tasma/TaskTest.java
@@ -24,12 +24,28 @@ public class TaskTest {
 
 	@Test
 	public void testGetStringEndDateTime1() {
-		Task t1 = new Task();
+		Task t = new Task();
 		DateTime dt = new DateTime(2015, 5, 6, 18, 20);
-		t1.setEndDateTime(dt);
-		String s = t1.getStringEndDateTime();
+		t.setEndDateTime(dt);
+		String s = t.getStringEndDateTime();
 		assertEquals(s, "06-05-15 at 6:20pm");
-		System.out.println(s);
 	}
 
+	@Test
+	public void testGetStringEndDateTime2() {
+		Task t = new Task();
+		DateTime dt = new DateTime(2015, 5, 6, 0, 0);
+		t.setEndDateTime(dt);
+		String s = t.getStringEndDateTime();
+		assertEquals(s, "06-05-15 at 12:00am"); // not implemented yet?
+	}
+	
+	@Test
+	public void testGetStringEndDateTime3() {
+		Task t = new Task();
+		DateTime dt = new DateTime(2015, 12, 13, 12, 30);
+		t.setEndDateTime(dt);
+		String s = t.getStringEndDateTime();
+		assertEquals(s, "13-12-15 at 12:30pm");
+	}
 }
