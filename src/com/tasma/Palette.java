ea5097c5419d0e37d61aaad5a4f30bb20bfbf687
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-01 11:58:48 +0800

    changed message info colour

--- src/com/tasma/Palette.java
+++ src/com/tasma/Palette.java
@@ -10,7 +10,7 @@ public class Palette {
 	public static final Color MESSAGE_SUCCESS = new Color(86, 179, 114);
 	public static final Color MESSAGE_WARNING = new Color(217, 217, 61);
 	public static final Color MESSAGE_DANGER = new Color(217, 71, 61);
-	public static final Color MESSAGE_INFO = THEME_BLUE;
+	public static final Color MESSAGE_INFO = new Color(21, 118, 179);
 	public static final Color MESSAGE_GRAYED = new Color(60, 60, 60);
 
 }
