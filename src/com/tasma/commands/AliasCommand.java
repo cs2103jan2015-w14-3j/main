c322b6695187c529acef9fc497fc364d94422d4b
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-01 13:39:42 +0800

    updated AliasCommand to cater for remove operation

--- src/com/tasma/commands/AliasCommand.java
+++ src/com/tasma/commands/AliasCommand.java
@@ -8,18 +8,27 @@ public class AliasCommand extends AbstractUndoableCommand {
 	private String key;
 	private String previousValue;
 	private String newValue;
+	private boolean isRemoveOperation = false;
 
 	public AliasCommand(TasmaUserInterface userInterface,
 			TaskCollection collection, String key, String value) {
 		super(userInterface, collection);
 		this.key = key.toLowerCase();
 		this.newValue = value;
+		if (this.key.equals("remove")) {
+			this.key = value;
+			isRemoveOperation = true;
+		}
 	}
 
 	@Override
 	public void execute() throws Exception {
 		previousValue = AliasHandler.getCustomAlias(key);
-		AliasHandler.setCustomAlias(key, newValue);
+		if (isRemoveOperation) {
+			AliasHandler.removeCustomAlias(key);
+		} else {
+			AliasHandler.setCustomAlias(key, newValue);
+		}
 	}
 
 	@Override
