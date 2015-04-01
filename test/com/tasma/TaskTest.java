e67af6d2815914aec7dab8da13c42276c82cb2e6
He Buwei <a0119434@nus.edu.sg>
2015-04-01 13:33:39 +0800

    add tests for Task.GetStringEndDateTime()

--- test/com/tasma/TaskTest.java
+++ test/com/tasma/TaskTest.java
@@ -6,10 +6,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.joda.time.DateTime;
+
 public class TaskTest {
 
+	Task task;
+	
 	@Before
 	public void setUp() throws Exception {
+		task = new Task();
 	}
 
 	@After
@@ -18,8 +23,13 @@ public class TaskTest {
 
 
 	@Test
-	public void testGetStringEndDateTime() {
-		
+	public void testGetStringEndDateTime1() {
+		Task t1 = new Task();
+		DateTime dt = new DateTime(2015, 5, 6, 18, 20);
+		t1.setEndDateTime(dt);
+		String s = t1.getStringEndDateTime();
+		assertEquals(s, "06-05-15 at 6:20pm");
+		System.out.println(s);
 	}
 
 }
