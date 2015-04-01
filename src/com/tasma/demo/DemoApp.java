4845872ad3afd9d0b9d84536d0274140ae2d8a60
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-01 22:25:36 +0800

    added demo sequence

--- src/com/tasma/demo/DemoApp.java
+++ src/com/tasma/demo/DemoApp.java
@@ -115,9 +115,219 @@ public class DemoApp implements Runnable {
 			actionsAvailable.offer(new Runnable() {
 				@Override
 				public void run() {
+					frame.displayMessage("");
 					frame.show();
 				}
 			});
+			
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("add");
+				}
+			});
+			
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("add return library book on next wednesday");
+				}
+			});
+			
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("add return library book on next wednesday");
+				}
+			});
+			
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("a buy birthday gift for hui qi");
+				}
+			});
+			
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("a buy birthday gift for hui qi");
+				}
+			});
+			
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("help");
+				}
+			});
+			
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("help add");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("help add");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("edit 5");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("edit 5");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("edit 5 buy a cushion as birthday gift for hui qi");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("edit 5 buy a cushion as birthday gift for hui qi");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("mark 4");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("mark 4");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("undo");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("undo");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("mark 5");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("mark 5");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					controller.executeInput("add do spring cleaning on Saturday");
+					controller.executeInput("add plan a short trip to batam");
+					controller.executeInput("add sign up for standard chartered marathon by next monday");
+					controller.executeInput("add send camera for repairs");
+					controller.executeInput("add get curry chicken from aunt for mum");
+					controller.executeInput("add take IPPT at Maju Camp");
+					controller.executeInput("add find cheap flights to Taiwan");
+					controller.executeInput("add get pants tailored for wedding dinner");
+					controller.executeInput("add try make DIY photo frames");
+					controller.executeInput("add buy contact lens solution");
+					controller.executeInput("add bring cousins out to dinner on sunday");
+					controller.executeInput("add pay for mobile bills");
+					controller.executeInput("add hire gardener to tidy up the lawn");
+					controller.executeInput("done 8");
+					
+					controller.executeInput("list");
+					frame.displayMessage("");
+					frame.editCmdDisplay("");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("search dinner");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("search dinner");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.displayMessage("");
+					frame.editCmdDisplay("delete 2");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.editCmdDisplay("");
+					controller.executeInput("delete 2");
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.hide();
+				}
+			});
+
+			actionsAvailable.offer(new Runnable() {
+				@Override
+				public void run() {
+					frame.show();
 				}
 			});
 		}
