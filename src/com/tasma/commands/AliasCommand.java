67d78d602ae5840d3b8939fa39be2ee349c19df1
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-01 13:00:56 +0800

    added AliasCommand

---
+++ src/com/tasma/commands/AliasCommand.java
@@ -0,0 +1,26 @@
+package com.tasma.commands;
+
+import com.tasma.TaskCollection;
+import com.tasma.TasmaUserInterface;
+
+public class AliasCommand extends AbstractUndoableCommand {
+
+	public AliasCommand(TasmaUserInterface userInterface,
+			TaskCollection collection) {
+		super(userInterface, collection);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void undo() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void execute() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
