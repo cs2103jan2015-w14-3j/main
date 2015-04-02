670e4ee952cade322ded138f7380b57da7158644
Edward <angertrail@hotmail.com>
2015-04-02 16:40:45 +0800

    Method to set the running of the scrollbars

--- src/com/tasma/LightScrollPane.java
+++ src/com/tasma/LightScrollPane.java
@@ -1,8 +1,48 @@
 package com.tasma;
 
+import java.awt.Container;
+
 import javax.swing.JComponent;
+import javax.swing.JLayeredPane;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneLayout;
+import javax.swing.SwingUtilities;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 
 public class LightScrollPane extends JComponent{
 	
+	private static final int SCROLL_BAR_ALPHA_ROLLOVER = 150;
+    private static final int SCROLL_BAR_ALPHA = 100;
+    private static final int THUMB_BORDER_SIZE = 2;
+    private static final int THUMB_SIZE = 8;
+    private static final Color THUMB_COLOR = Color.BLACK;
+
+    private final JScrollPane scrollPane;
+    private final JScrollBar verticalScrollBar;
+    private final JScrollBar horizontalScrollBar;
+    
+    public LightScrollPane(JComponent component) {
+        scrollPane = new JScrollPane(component);
+        verticalScrollBar = scrollPane.getVerticalScrollBar();
+        verticalScrollBar.setVisible(false);
+        verticalScrollBar.setOpaque(false);
+        verticalScrollBar.setUI(new MyScrollBarUI());
+
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setLayer(verticalScrollBar, JLayeredPane.PALETTE_LAYER);
+
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setLayout(new ScrollPaneLayout() {
+            @Override
+            public void layoutContainer(Container parent) {
+                viewport.setBounds(0, 0, getWidth(), getHeight());
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        displayScrollBarsIfNecessary(viewport);
+                    }
+                });
+            }
+        });
 }
