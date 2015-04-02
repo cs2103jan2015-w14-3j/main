95b9cc4aa29e0131a472bb37e691c923a18dae7c
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-02 22:01:15 +0800

    added placeholder text blue

--- src/com/tasma/Palette.java
+++ src/com/tasma/Palette.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 public class Palette {
 
 	public static final Color THEME_BLUE = new Color(41, 171, 226);
+	public static final Color PLACEHOLDER_TEXT = new Color(162, 216, 232);
 	
 	public static final Color MESSAGE_DEFAULT = new Color(20, 20, 20);
 	public static final Color MESSAGE_SUCCESS = new Color(86, 179, 114);
