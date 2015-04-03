58edaffd73a02ea28161f22eeed2290b13769125
Edward <angertrail@hotmail.com>
2015-04-03 11:51:45 +0800

    Constructors to set the limit by chars

--- src/com/tasma/JTextFieldLimit.java
+++ src/com/tasma/JTextFieldLimit.java
@@ -1,5 +1,17 @@
 package com.tasma;
 
-public class JTextFieldLimit {
+import javax.swing.text.PlainDocument;
 
+public class JTextFieldLimit extends PlainDocument{
+	private int limit;
+	
+	JTextFieldLimit(int limit) {
+		super();
+		this.limit = limit;
+	}
+	
+	JTextFieldLimit(int limit, boolean upper) {
+	    super();
+	    this.limit = limit;
+	}
 }
