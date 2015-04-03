8d1b69a9cdbebcdeea4a30f102ce1317cd765cdc
Edward <angertrail@hotmail.com>
2015-04-03 11:52:54 +0800

    Method to include string and set length

--- src/com/tasma/JTextFieldLimit.java
+++ src/com/tasma/JTextFieldLimit.java
@@ -1,8 +1,14 @@
 package com.tasma;
 
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 
 public class JTextFieldLimit extends PlainDocument{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int limit;
 	
 	JTextFieldLimit(int limit) {
@@ -13,5 +19,14 @@ public class JTextFieldLimit extends PlainDocument{
 	JTextFieldLimit(int limit, boolean upper) {
 	    super();
 	    this.limit = limit;
+	}
+	
+	 public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
+		    if (str == null)
+		      return;
+
+		    if ((getLength() + str.length()) <= limit) {
+		      super.insertString(offset, str, attr);
+		    }
 	}
 }
