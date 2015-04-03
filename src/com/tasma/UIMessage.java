236a8c135fa7df874fb56cf99ad6c5d4fe19a0b9
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-03 14:58:14 +0800

    updated UI messages

--- src/com/tasma/UIMessage.java
+++ src/com/tasma/UIMessage.java
@@ -6,25 +6,25 @@ package com.tasma;
 
 public final class UIMessage {
 	public static final String COMMAND_ADD_SUCCESS = "Your task \"%s\" has been successfully created.";
-	public static final String COMMAND_ADD_ARG_EMPTY = "The 'add' command requires the details of the task you wish to add. Try:\n\tadd <details>";
+	public static final String COMMAND_ADD_ARG_EMPTY = "The 'add' command requires the details of the task you wish to add. Try:\n\tadd <details>\nIf you need help, you can type `help add` to get help on the command.";
 	public static final String COMMAND_ADD_UNDO = "Your task \"%s\" has been undone.";
 	
 	public static final String COMMAND_MARK_SUCCESS = "Your task \"%s\" has been marked as done.";
 	public static final String COMMAND_MARK_NOTFOUND = "The task you were looking to mark done was not found.";
-	public static final String COMMAND_MARK_ARG_EMPTY = "The 'mark' command requires the task ID of the task you wish to mark done. Try:\n\tmark <number>";
+	public static final String COMMAND_MARK_ARG_EMPTY = "The 'mark' command requires the task ID of the task you wish to mark done. Try:\n\tmark <number>\nIf you need help, you can type `help mark` to get help on the command.";
 	public static final String COMMAND_MARK_UNDO = "Your task \"%s\" has been marked as undone.";
 
 	public static final String COMMAND_DELETE_SUCCESS = "Your task \"%s\" has been deleted.";
 	public static final String COMMAND_DELETE_NOTFOUND = "The task you were looking to delete was not found.";
-	public static final String COMMAND_DELETE_ARG_EMPTY = "The 'delete' command requires the task ID of the task you wish to remove. Try:\n\tdelete <number>";
+	public static final String COMMAND_DELETE_ARG_EMPTY = "The 'delete' command requires the task ID of the task you wish to remove. Try:\n\tdelete <number>\nIf you need help, you can type `help delete` to get help on the command.";
 	public static final String COMMAND_DELETE_UNDO = "Your task \"%s\" has been undeleted.";
 
 	public static final String COMMAND_EDIT_SUCCESS = "Your task \"%s\" has been successfully updated.";
 	public static final String COMMAND_EDIT_NOTFOUND = "The task you were looking to edit was not found.";
-	public static final String COMMAND_EDIT_ARG_EMPTY = "The 'edit' command requires the task ID and details of the task you wish to update with. Try:\n\tedit <number> <details>";
+	public static final String COMMAND_EDIT_ARG_EMPTY = "The 'edit' command requires the task ID and details of the task you wish to update with. Try:\n\tedit <number> <details>\nIf you need help, you can type `help edit` to get help on the command.";
 	public static final String COMMAND_EDIT_UNDO = "Changes to your task \"%s\" has been undone.";
 
-	public static final String COMMAND_SEARCH_EMPTY_QUERY = "The 'search' command requires a query to find tasks for you. Enter the `help search` command for more information on 'search'.";
+	public static final String COMMAND_SEARCH_EMPTY_QUERY = "The 'search' command requires a query to find tasks for you. Enter the `help search` command for more information on 'search'.\nIf you need help, you can type `help search` to get help on the command.";
 	public static final String COMMAND_SEARCH_RESULT = "%d result(s) found for \"%s\".";
 
 	public static final String COMMAND_LIST_RESULT = "%d task(s) upcoming.";
