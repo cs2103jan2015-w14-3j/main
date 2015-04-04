51031c8bbbb6ae65b1540a55088fb0d56d8e0944
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-04 16:01:17 +0800

    added javadoc for UIMessage

--- src/com/tasma/UIMessage.java
+++ src/com/tasma/UIMessage.java
@@ -4,6 +4,10 @@
 //@author A0132763
 package com.tasma;
 
+/**
+ * Provides UI messages
+ * @author Yong Shan Xian <ysx@u.nus.edu>
+ */
 public final class UIMessage {
 	public static final String COMMAND_ADD_SUCCESS = "Your task \"%s\" has been successfully created.";
 	public static final String COMMAND_ADD_ARG_EMPTY = "The 'add' command requires the details of the task you wish to add. Try:\n\tadd <details>\nIf you need help, you can type `help add` to get help on the command.";
