3c04e476c230be4dfea0da2dc79befd217813cf0
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-08 01:48:11 +0800

    added invalidcommand test

---
+++ test/com/tasma/commands/InvalidCommandTest.java
@@ -0,0 +1,45 @@
+/**
+ * Tasma Task Manager
+ */
+//@author A0132763H
+package com.tasma.commands;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.tasma.MockStorage;
+import com.tasma.MockUserInterface;
+import com.tasma.TaskCollection;
+import com.tasma.UIMessage;
+
+public class InvalidCommandTest {
+	
+	private MockStorage storage;
+	
+	private TaskCollection collection;
+	
+	private MockUserInterface userInterface;
+
+	@Before
+	public void setUp() throws Exception {
+		userInterface = new MockUserInterface();
+		storage = new MockStorage();
+		collection = new TaskCollection(storage);
+		collection.loadFromFile();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testExecute() throws Exception {
+		InvalidCommand command = new InvalidCommand(userInterface, collection, "some command");
+		command.execute();
+		assertEquals(UIMessage.COMMAND_INVALID, userInterface.getLastDisplayedMessage());
+	}
+
+}
