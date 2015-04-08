aba65c2dec118e44bf9423adc2721fa2bc522088
He Buwei <a0119434@nus.edu.sg>
2015-04-09 07:31:43 +0800

    some refactoring of notification

--- src/com/tasma/BalloonNotification.java
+++ src/com/tasma/BalloonNotification.java
@@ -12,36 +12,34 @@ import java.util.TimerTask;
 import org.joda.time.DateTime;
 
 public class BalloonNotification {
-	
+
 	private static final String REMINDER_CAPTION = "Reminder";
-	private static final String REMINDER_DEADLINE = "Reminder";
-	private static final String REMINDER_TIMED = "Reminder";
-	
+	private static final String REMINDER_DEADLINE = "The task \"%s\" is due in 1 hour";
+	private static final String REMINDER_TIMED = "The task \"%s\" starts in 1 hour";
+
 	private Timer timer;
 	private TaskCollection collection;
 	private TrayIcon trayIcon;
-	
+
 	BalloonNotification(TaskCollection collection, TrayIcon trayIcon) {
 		timer = new Timer();
 		this.collection = collection;
 		this.trayIcon = trayIcon;
 	}
-	
-	public void setup(){
+
+	public void setup() {
 		collection.notification = this;
 	}
-	
-	public void scheduleNotifications(List<Task> undoneTasks){
+
+	public void updateNotifications(List<Task> undoneTasks) {
 		timer.cancel();
 		timer = new Timer();
-		for(Task task : undoneTasks){
+		for (Task task : undoneTasks) {
 			TaskType type = task.getType();
-			switch(type){
+			switch (type) {
 			case DEADLINE:
-				scheduleDeadlineNotification(task);
-				break;
 			case TIMED:
-				scheduleTimedTaskNotification(task);
+				scheduleNotification(task, type);
 				break;
 			case FLOATING:
 			default:
@@ -49,40 +47,36 @@ public class BalloonNotification {
 			}
 		}
 	}
-	
-	private void scheduleDeadlineNotification(Task task) {
-		
-		String taskDetail = task.getDetails();
-		DateTime startDateTime = task.getStartDateTime();
-		
-		Date reminderTime = startDateTime.minusHours(1).toDate();
-		
-		TimerTask timertask = new TimerTask() {
-			@Override
-			public void run() {
-				trayIcon.displayInfo(REMINDER_CAPTION, "The task \"" + taskDetail + "\" is due in 1 hour");
-				System.out.println("deadline reminder");
-			}
-		};
-		timer.schedule(timertask, reminderTime);
-		System.out.println("deadline reminder scheduled at " + reminderTime.toString());
-	}
-	
-	private void scheduleTimedTaskNotification(Task task) {
-		String taskDetail = task.getDetails();
-		DateTime startDateTime = task.getStartDateTime();
-		
-		Date reminderTime = startDateTime.minusHours(1).toDate();
-		
-		TimerTask timertask = new TimerTask() {
-			@Override
-			public void run() {
-				trayIcon.displayInfo(REMINDER_CAPTION, "The task \"" + taskDetail + "starts in 1 hour");
-				System.out.println("timed task reminder");
-			}
-		};
-		timer.schedule(timertask, reminderTime);
-		System.out.println("timed task reminder scheduled at " + reminderTime.toString());
+
+	private class Reminder extends TimerTask {
+
+		String reminderMessage;
+
+		Reminder(String message) {
+			reminderMessage = message;
+		}
+
+		@Override
+		public void run() {
+			trayIcon.displayInfo(REMINDER_CAPTION, reminderMessage);
+		}
 	}
 
+	private void scheduleNotification(Task task, TaskType type) {
+
+		String taskDetail = task.getDetails();
+		DateTime startDateTime = task.getStartDateTime();
+		Date reminderTime = startDateTime.minusHours(1).toDate();
+		String reminderMessage = "";
+
+		if (type == TaskType.DEADLINE) {
+			reminderMessage = String.format(REMINDER_DEADLINE, taskDetail);
+		} else if (type == TaskType.TIMED) {
+			reminderMessage = String.format(REMINDER_TIMED, taskDetail);
+		}
+
+		TimerTask reminder = new Reminder(reminderMessage);
+		timer.schedule(reminder, reminderTime);
+
+	}
 }
