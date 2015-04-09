15d4116161f4760448948296819454ec31981cb1
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-09 14:43:54 +0800

    mark command handling invalid index

--- src/com/tasma/commands/MarkCommand.java
+++ src/com/tasma/commands/MarkCommand.java
@@ -20,25 +20,37 @@ public class MarkCommand extends AbstractUndoableCommand {
 	public MarkCommand(TasmaUserInterface userInterface,
 			TaskCollection collection, List<Task> state, int index) {
 		super(userInterface, collection);
-		this.state = state;
-		this.task = state.get(index);
+		try {
+			this.state = state;
+			this.task = state.get(index);
+		} catch (NullPointerException ex) {
+			userInterface.displayMessage(UIMessage.COMMAND_MARK_ARG_EMPTY, Palette.MESSAGE_WARNING);
+		}
 	}
 
 	@Override
 	public void execute() throws Exception {
-		task.setDone(true);
-		collection.update(task);
-		userInterface.displayMessage(String.format(UIMessage.COMMAND_MARK_SUCCESS, task.getDetails()), Palette.MESSAGE_SUCCESS);
-		
-		ListCommand listCommand = new ListCommand(userInterface, collection, state);
-		listCommand.execute();
+		if (task == null) {
+			throw new NotExecutedException();
+		} else {
+			task.setDone(true);
+			collection.update(task);
+			userInterface.displayMessage(String.format(UIMessage.COMMAND_MARK_SUCCESS, task.getDetails()), Palette.MESSAGE_SUCCESS);
+			
+			ListCommand listCommand = new ListCommand(userInterface, collection, state);
+			listCommand.execute();
+		}
 	}
 
 	@Override
 	public void undo() throws Exception {
-		task.setDone(false);
-		collection.update(task);
-
-		userInterface.displayMessage(String.format(UIMessage.COMMAND_MARK_UNDO, task.getDetails()), Palette.MESSAGE_SUCCESS);
+		if (task == null) {
+			throw new NotExecutedException();
+		} else {
+			task.setDone(false);
+			collection.update(task);
+	
+			userInterface.displayMessage(String.format(UIMessage.COMMAND_MARK_UNDO, task.getDetails()), Palette.MESSAGE_SUCCESS);
+		}
 	}
 }
