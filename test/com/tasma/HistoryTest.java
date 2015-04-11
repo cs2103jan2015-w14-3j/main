ff30dc73539ca7c2bbef8d7a7b7ed35fb362b22c
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-11 22:15:04 +0800

    added test for History class

--- test/com/tasma/HistoryTest.java
+++ test/com/tasma/HistoryTest.java
@@ -2,14 +2,31 @@ package com.tasma;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.tasma.commands.AddCommand;
+import com.tasma.ui.MockUserInterface;
+
 public class HistoryTest {
+	
+	private History history;
+	private MockUserInterface userInterface;
+	private TaskCollection collection;
+	private MockStorage storage;
+	private LinkedList<Task> state;
 
 	@Before
 	public void setUp() throws Exception {
+		history = new History();
+		userInterface = new MockUserInterface();
+		storage = new MockStorage();
+		collection = new TaskCollection(storage);
+		collection.loadFromFile();
+		state = new LinkedList<Task>();
 	}
 
 	@After
@@ -17,23 +34,49 @@ public class HistoryTest {
 	}
 
 	@Test
-	public void testOffer() {
-		fail("Not yet implemented");
+	public void testOffer() throws Exception {
+		AddCommand command = new AddCommand(userInterface, collection, state, "test");
+		command.execute();
+		assertEquals(1, collection.tasks.size());
+		history.offer("add test", command);
+		assertEquals(1, history.undoStack.size());
+		assertEquals(command, history.undoStack.peek());
 	}
 
 	@Test
-	public void testUndo() {
-		fail("Not yet implemented");
+	public void testUndo() throws Exception {
+		AddCommand command = new AddCommand(userInterface, collection, state, "test");
+		command.execute();
+		assertEquals(1, collection.tasks.size());
+		history.offer("add test", command);
+		history.undo();
+		assertEquals(0, collection.tasks.size());
+		assertEquals(1, history.redoStack.size());
+		assertEquals(command, history.redoStack.peek());
 	}
 
 	@Test
-	public void testRedo() {
-		fail("Not yet implemented");
+	public void testRedo() throws Exception {
+		AddCommand command = new AddCommand(userInterface, collection, state, "test");
+		command.execute();
+		assertEquals(1, collection.tasks.size());
+		history.offer("add test", command);
+		history.undo();
+		history.redo();
+		assertEquals(1, collection.tasks.size());
+		assertEquals(1, history.undoStack.size());
+		assertEquals(command, history.undoStack.peek());
 	}
 
 	@Test
-	public void testPopLastInput() {
-		fail("Not yet implemented");
+	public void testPopLastInput() throws Exception {
+		AddCommand command = new AddCommand(userInterface, collection, state, "test");
+		command.execute();
+		assertEquals(1, collection.tasks.size());
+		history.offer("add test", command);
+		assertEquals(1, history.inputStack.size());
+		assertEquals("add test", history.popLastInput());
+		assertEquals(0, history.inputStack.size());
 	}
 
 }
