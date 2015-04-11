84728d31181ffbda82ddf92495e9f8b26708cf0d
Sam-Mauris Yong <mauris@hotmail.sg>
2015-04-11 21:39:58 +0800

    added historytest file

---
+++ test/com/tasma/HistoryTest.java
@@ -0,0 +1,39 @@
+package com.tasma;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HistoryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testOffer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUndo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRedo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPopLastInput() {
+		fail("Not yet implemented");
+	}
+
+}
